efa21c59a38461be8d931aefc53ed69e046ea811
neetarawat1166 <neetarawat1166@gmail.com>
2023-03-25 02:37:14 +0530

    first commit

---
+++ lib/views/subject_list.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/views/quiz_screen.dart';
+
+
+class SubjectListScreen extends StatelessWidget {
+  const SubjectListScreen({super.key});
+
+  final List subjectListData = const [
+   
+    {"subject_name": "C++", "subject_id": "1003"},
+    {"subject_name": "C Programming", "subject_id": "1004"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Subject List"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            ...subjectListData
+                .map(
+                  (e) => Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: ListTile(
+                      onTap: () {
+                        print(e['subject_id']);
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const QuizScreen(),
+                          ),
+                        );
+                      },
+                      tileColor: Colors.teal,
+                      textColor: Colors.white,
+                      iconColor: Colors.white,
+                      title: Text(
+                        e['subject_name'],
+                      ),
+                      trailing: const Icon(Icons.arrow_forward),
+                    ),
+                  ),
+                )
+                .toList()
+          ],
+        ),
+      ),
+    );
+  }
+}
